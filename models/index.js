069e2731f1d5f63f5815ab8879947f6f21fa77be
millernj <noahjmiller17@gmail.com>
2020-06-02 20:14:09 -0700

    adding basic folder structure for a mern app

---
+++ models/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  Post: require("./posts")
+};
---
+++ scripts/seedDB.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/presume"
+);
+
+const PostSeed = [
+  // stuff
+];
+
+db.Post.remove({})
+  .then(() => db.Post.collection.insertMany(postSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
