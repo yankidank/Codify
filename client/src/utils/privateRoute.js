a2209eabd966e80d786d54a3f9c0dc14fec9e079
elliotfouts <elliotfouts@gmail.com>
2020-06-06 23:11:54 -0700

    added prop validation for eslint

--- client/src/utils/privateRoute.js
+++ client/src/utils/privateRoute.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import PropTypes from 'prop-types';
 
-export const PrivateRoute = ({ component: Component, ...rest }) => {
-    let isLoggedIn = true;
+
+export function PrivateRoute ({component: Component, ...rest }) {
+    let isLoggedIn = false;
     // get("localhost:3001/api/getUserInfo")
     //      .then(result => isLoggedIn = result);
     //  
     return <Route {...rest} render={(props) => ( isLoggedIn ? <Component {...props} /> : <Redirect to='/login' /> )} />
+}
+
+PrivateRoute.propTypes = {
+    component: PropTypes.any
 }
     
