e2a7d2ceaa38acd044c86311c6319a91da40a621
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-06-09 10:50:23 -0700

    Login Menu style updates

--- client/src/components/LoginMenu/MenuLogin.js
+++ client/src/components/LoginMenu/MenuLogin.js
@@ -1,25 +1,46 @@
 import React from "react";
+import NavBar from "../NavBar"
 
+const handleGoogleLogin = () => {
+    window.open("http://localhost:3001/auth/google", "_self")
+}
+const handleGithubLogin = () => {
+    window.open("http://localhost:3001/auth/github", "_self")
+}
+const handleLinkedInLogin = () => {
+    window.open("http://localhost:3001/auth/linkedin", "_self")
+}
+const handleHome = () => {
+    window.open("/", "_self")
+}
+const handleAbout = () => {
+    window.open("/about", "_self")
+}
 function MenuLogin(){
     return (
-        <div className="menuLogin">
-            <ul>
-                <li><button className="btn-home-login">HOME</button></li>
-                <li><button className="btn-home-login">ABOUT</button></li>
-                <li><button onClick={handleGithubLogin} className="btn-github">
-                    <div><img src="./assets/img/icon-github.png" style={{height: "25px"}}></img></div>
-                    <div>Login with GitHub</div>
-                    </button></li>
-                <li><button onClick={handleLinkedInLogin} className="btn-linkedin">
-                    <div><img src="./assets/img/icon-linkedin.png" style={{height: "25px"}}></img></div>
-                    <div>Login with LinkedIn</div>
-                    </button></li>
-                <li><button onClick={handleGoogleLogin} className="btn-google">
-                    <div><img src="./assets/img/icon-google.png" style={{height: "25px"}}></img></div>
-                    <div>Login with Google</div>
-                    </button></li>
-                <li><button onClick={handleLogout}>LOGOUT</button></li>
-            </ul>
+        <div>
+            <NavBar />
+            <div className="menuLogin">
+                <ul>
+                    <li><button onClick={handleHome} className="btn-home-login">HOME</button></li>
+                    <li><button onClick={handleAbout} className="btn-home-login">ABOUT</button></li>
+                    <li><button onClick={handleGithubLogin} className="btn-github">
+                        <div className="btn-logo"><img src="./assets/img/icon-github.png"></img></div>
+                        <div className="btn-title">Login with GitHub</div>
+                        </button>
+                    </li>
+                    <li><button onClick={handleLinkedInLogin} className="btn-linkedin">
+                        <div className="btn-logo"><img src="./assets/img/icon-linkedin.png"></img></div>
+                        <div className="btn-title">Login with LinkedIn</div>
+                        </button>
+                    </li>
+                    <li><button onClick={handleGoogleLogin} className="btn-google">
+                        <div className="btn-logo"><img src="./assets/img/icon-google.png"></img></div>
+                        <div className="btn-title">Login with Google</div>
+                        </button>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
