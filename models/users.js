2e68303c6657436ac4daefa4e1d30d9a9fcf9d84
millernj <noahjmiller17@gmail.com>
2020-06-09 21:25:32 -0700

    adding user schema and seed to test

---
+++ models/users.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const SALT_WORK_FACTOR = 10;
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  // stuff here
+  username: {
+    type: String,
+    required: true,
+    index: { unique: true }
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  source: {
+    type: String,
+    default: 'local'
+  },
+  oauth: String
+}, { timestamps: true });
+
+userSchema.path('email').validate(function (email) {
+  var emailRegex = /^([\w-.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  return emailRegex.test(email);
+}, 'The e-mail field must be valid')
+
+userSchema.pre("save", function(next) {
+  let user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+
+        // override the cleartext password with the hashed one
+        user.password = hash;
+        next();
+    });
+});
+});
+
+userSchema.methods.comparePassword = function(plaintext, callback) {
+  return callback(null, bcrypt.compareSync(plaintext, this.password));
+};
+
+const User = mongoose.model("user", userSchema);
+
+
+module.exports = User;
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -4,20 +4,53 @@ const db = require("../models");
 // This file empties the Books collection and inserts the books below
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/presume"
+  process.env.MONGODB_URI || "mongodb://localhost/presume",
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true 
+  }
 );
 
-const postSeed = [
-  // stuff
-];
+const userSeed = new db.User({
+  username: 'some guy',
+  email: 'some.guy@gmail.com',
+  password: 'some password'
+})
 
-db.Post.remove({})
-  .then(() => db.Post.collection.insertMany(postSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
+db.User.deleteMany({})
+  .then(() => {
+    return userSeed.save(err => {
+      if (err) throw err;
+
+      db.User.findOne({ username: 'some guy' }, function (err, user) {
+        if (err) throw err;
+
+        // test a matching password
+        user.comparePassword('some password', function (err, isMatch) {
+          if (err) throw err;
+          console.log('some password:', isMatch); // -> some password: true
+        });
+
+        // test a failing password
+        user.comparePassword('anything else', function (err, isMatch) {
+          if (err) throw err;
+          console.log('anything else:', isMatch); // -> anything else: false
+        });
+
+        process.exit(0);
+
+      });
+    });
   })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+
+// db.Post.remove({})
+//   .then(() => db.Post.collection.insertMany(postSeed))
+//   .then(data => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+//   });
