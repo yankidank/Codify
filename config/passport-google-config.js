b697e34225e6169589be61a25fbc9c56dea22cd9
elliotfouts <elliotfouts@gmail.com>
2020-06-11 16:10:16 -0700

    connected google oauth route to user model

--- config/passport-google-config.js
+++ config/passport-google-config.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../models/users');
 
 // Google Oauth strategy configuration
 passport.use(
@@ -10,18 +11,23 @@ passport.use(
 			callbackURL: 'http://localhost:3001/auth/google/callback'
 		},
 		function (accessToken, refreshToken, profile, done) {
-			console.log(profile);
-
-			// ^^ this profile parameter contains all of the information from google that should be stored in the database
-			// This is the information that should be stored in the database.
-			// When MongoDB returns the new user, it should be passed to the done method (I have created a user object because we don't have a User Schema yet)
 			const { id, displayName, emails } = profile;
-			let newUser = {
-				googleID: id,
-				displayName,
-				email: emails[0].value
-			};
-			done(null, newUser);
+			// check if user is already in database
+			User.find({ google: { id: id } }, (err, user) => {
+				// if user already exists
+				if (user[0]) {
+					done(null, user[0]);
+				} else {
+					// create new user
+					User.create({ displayName, email: emails[0].value, google: { id } }, (err, user) => {
+						if (err) {
+							done(err, false);
+						} else {
+							done(null, user);
+						}
+					});
+				}
+			});
 		}
 	)
 );
