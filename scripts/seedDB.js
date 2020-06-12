f68baa00a55b12837d097f3a67c2371588b068f7
millernj <noahjmiller17@gmail.com>
2020-06-11 22:07:31 -0700

    adding seeding for companies, contacts, and jobs

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { Company, User } = require("../models");
+const { Company, Contact, User, Job } = require("../models");
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/presume",
@@ -17,24 +17,12 @@ const companySeed = [
       street: '1409 S Saltair Ave. APT 303',
       city: 'Los Angeles',
       state: 'CA',
-      zip: '90025'
+      zip: 90025
     },
     description: 'His actual house',
     notes: 'Please call before you visit'
   }
 ]
-
-Company.deleteMany({})
-  .then(async () => {
-    try {
-      const companies = await Company.create(companySeed);
-      console.log(companies);
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
-  })
-
 
 const userSeed = [
   {
@@ -49,26 +37,117 @@ const userSeed = [
   }
 ]
 
-User.deleteMany({})
-  .then(async () => {
-    try {
-      const users = await User.create(userSeed);
-      console.log(users);
-      const someGuy = await User.findOne({ displayName: 'some guy' });
+const contactSeed = (userId, companyId) => [
+  {
+    userId,
+    companyId,
+    displayName: "some dude i dunno",
+    email: 'some.dude@gmail.com',
+    phone: '8888888888',
+    position: 'the dude',
+    notes: 'dude why'
+  }
+]
 
-      someGuy.comparePassword('some password', function (err, isMatch) {
-        if (err) throw err;
-        console.log('some password:', isMatch); // -> some password: true
-      });
-      someGuy.comparePassword('anything else', function (err, isMatch) {
-        if (err) throw err;
-        console.log('anything else:', isMatch); // -> anything else: false
-      });
+const jobSeed = (userId, companyId, contactIds) => [
+  {
+    userId,
+    companyId,
+    contactIds,
+    name: 'Google Software Developer',
+    interviews: [
+      {
+        date: new Date(),
+        location: {
+          remote: true,
+          street: '12360 Landale St.',
+          city: 'Studio City',
+          state: 'CA',
+          zip: 91604
+        }
+      }
+    ],
+    post: {
+      date: new Date(),
+      position: 'Junior Developer',
+      city: 'Santa Monica',
+      state: 'CA',
+      salary: 80000,
+      bonus: 10000,
+      notes: 'Looks swanky'
+    },
+    offers: [
+      {
+          date: new Date(),
+          startDate: new Date('2020-10-07'),
+          salary: 80000,
+          bonus: 10000,
+          benefits: 'Transamerica Retirement Plan'
+      }
+    ],
+    status: 'Saved'
+  },
+]
 
-      process.exit(0);
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
+const seedUsers = async (userSeed) => {
+  const users = await User.create(userSeed);
+  const someGuy = await User.findOne({ displayName: 'some guy' });
+
+  someGuy.comparePassword('some password', function (err, isMatch) {
+    if (err) throw err;
+    console.log('some password:', isMatch); // -> some password: true
   });
+  someGuy.comparePassword('anything else', function (err, isMatch) {
+    if (err) throw err;
+    console.log('anything else:', isMatch); // -> anything else: false
+  });
+  console.log(`Added ${users.length} users`)
+  return users;
+}
 
+const seedCompanies = async (companySeed) => {
+  const companies = await Company.create(companySeed);
+  console.log(`Added ${companies.length} companies`)
+  return companies;
+}
+
+const seedContacts = async (contactSeed) => {
+  const contacts = await Contact.create(contactSeed);
+  console.log(`Added ${contacts.length} contacts`)
+  return contacts;
+}
+
+const seedJobs = async (jobSeed) => {
+  const jobs = await Job.create(jobSeed);
+  console.log(`Added ${jobs.length} jobs`)
+  return jobs;
+}
+
+const seedAll = async () => {
+  try {
+    await Promise.all([
+      User.deleteMany({}),
+      Company.deleteMany({}),
+      Contact.deleteMany({}),
+      Job.deleteMany({})
+    ]);
+
+    const users = await seedUsers(userSeed);
+    const companies = await seedCompanies(companySeed);
+
+    const someGuysId = users[0]._id;
+    const someCompanyIds = [companies[0]._id];
+
+    const contacts = await seedContacts(contactSeed(someGuysId, someCompanyIds));
+    const someContactIds = [contacts[0]._id];
+
+    const jobs = await seedJobs(jobSeed(someGuysId, someCompanyIds[0], someContactIds));
+
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+seedAll();
