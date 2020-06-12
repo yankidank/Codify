d3029136e0aa4977cb4db2289ab029459e7000d7
millernj <noahjmiller17@gmail.com>
2020-06-11 20:56:07 -0700

    adding a company seed

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,56 +1,74 @@
 const mongoose = require("mongoose");
-const db = require("../models");
-
-// This file empties the Books collection and inserts the books below
+const { Company, User } = require("../models");
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/presume",
-  { 
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true 
+    useCreateIndex: true
   }
 );
 
-const userSeed = new db.User({
-  displayName: 'some guy',
-  email: 'some.guy@gmail.com',
-  password: 'some password'
-})
+const companySeed = [
+  {
+    displayName: 'Noah Miller\'s House',
+    location: {
+      street: '1409 S Saltair Ave. APT 303',
+      city: 'Los Angeles',
+      state: 'CA',
+      zip: '90025'
+    },
+    description: 'His actual house',
+    notes: 'Please call before you visit'
+  }
+]
 
-db.User.find({})
-  .then(() => {
-    return userSeed.save(err => {
-      if (err) throw err;
-
-      db.User.findOne({ displayName: 'some guy' }, function (err, user) {
-        if (err) throw err;
-
-        // test a matching password
-        user.comparePassword('some password', function (err, isMatch) {
-          if (err) throw err;
-          console.log('some password:', isMatch); // -> some password: true
-        });
-
-        // test a failing password
-        user.comparePassword('anything else', function (err, isMatch) {
-          if (err) throw err;
-          console.log('anything else:', isMatch); // -> anything else: false
-        });
-
-        process.exit(0);
-
-      });
-    });
+Company.deleteMany({})
+  .then(async () => {
+    try {
+      const companies = await Company.create(companySeed);
+      console.log(companies);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
   })
 
-// db.Post.remove({})
-//   .then(() => db.Post.collection.insertMany(postSeed))
-//   .then(data => {
-//     console.log(data.result.n + " records inserted!");
-//     process.exit(0);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//     process.exit(1);
-//   });
+
+const userSeed = [
+  {
+    displayName: 'some guy',
+    email: 'some.guy@gmail.com',
+    password: 'some password'
+  },
+  {
+    displayName: 'some other guy',
+    email: 'some.other.guy@gmail.com',
+    password: 'some password'
+  }
+]
+
+User.deleteMany({})
+  .then(async () => {
+    try {
+      const users = await User.create(userSeed);
+      console.log(users);
+      const someGuy = await User.findOne({ displayName: 'some guy' });
+
+      someGuy.comparePassword('some password', function (err, isMatch) {
+        if (err) throw err;
+        console.log('some password:', isMatch); // -> some password: true
+      });
+      someGuy.comparePassword('anything else', function (err, isMatch) {
+        if (err) throw err;
+        console.log('anything else:', isMatch); // -> anything else: false
+      });
+
+      process.exit(0);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  });
+
