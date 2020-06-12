71aa6a40bd7b0a0ffb745eca1c143936d2846136
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-06-11 19:21:11 -0700

    Moved filter into it's own component

--- client/src/components/JobList/List.js
+++ client/src/components/JobList/List.js
@@ -1,4 +1,5 @@
-import React, {useEffect} from "react";
+import React from "react";
+import Filter from "./Filter"
 import OneJobListing from "./JobListItem"
 
 const handleAdd = () => {
@@ -6,17 +7,6 @@ const handleAdd = () => {
 }
 
 function List(props){
-
-    useEffect(() => {
-        // Toggle Filter Visibility
-        const dropdown = document.getElementById("filter-toggle");
-        const container = document.getElementById("filter-container");
-        dropdown.addEventListener('click', function() { 
-            dropdown.classList.toggle('filter-active');
-            container.classList.toggle('hidden');
-        }, false);
-    });
-
     return (
         <div className={props.cols}>
             <div className="card oneJobListing">
@@ -25,31 +15,8 @@ function List(props){
                     <span className="card-button" onClick={handleAdd}>Add New Job</span>
                 </div>
                 <div className="container container-full ">
-                    <div className="row row-filter">
-                        <div className="col l12 card-filter" id="filter-toggle">
-                            Filter <i className="material-icons">keyboard_arrow_down</i>
-                        </div>
-                    </div>
-                    <div className="row hidden" id="filter-container">
-                        <div className="col l12 filter-tags">
-                            <div className="btn-filter" id="filter-saved">
-                                <button className="btn-saved">Saved</button>
-                            </div>
-                            <div className="btn-filter" id="filter-applied">
-                                <button className="btn-applied">Applied</button>
-                            </div>
-                            <div className="btn-filter" id="filter-interview">
-                                <button className="btn-interview">Interview</button>
-                            </div>
-                            <div className="btn-filter" id="filter-offer">
-                                <button className="btn-offer">Offer</button>
-                            </div>
-                            <div className="btn-filter" id="filter-ended">
-                                <button className="btn-ended">Ended</button>
-                            </div>
-                        </div>
-                    </div>
-                     <OneJobListing />
+                    <Filter />
+                    <OneJobListing />
                 </div>
             </div>
         </div>
