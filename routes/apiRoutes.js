d15a3bbb9b5782f8cdf89f4ac5c5267e6df63cb8
millernj <noahjmiller17@gmail.com>
2020-06-13 11:58:31 -0700

    adding get routes to job controller

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
-const { users, companies, contacts } = require('../controllers');
+const { users, companies, contacts, jobs } = require('../controllers');
 
 router.use('/users', users);
 router.use('/companies', companies);
 router.use('/contacts', contacts);
+router.use('/jobs', jobs);
 
 module.exports = router;
