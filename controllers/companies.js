088538a8ee10aab6915b97f284715a2c3e0ba023
elliotfouts <elliotfouts@gmail.com>
2020-06-12 21:06:58 -0700

    added company controller

--- controllers/companies.js
+++ controllers/companies.js
@@ -1,24 +1,79 @@
 const { Router } = require('express');
 const { Company } = require('../models');
 const { buildFilter } = require('../utils/queryHelper');
+const { dropUndefined } = require('../utils/dropUndefined');
 
 const router = Router();
 
 router.get('/', async (request, response) => {
-  const {
-    query: { displayName, description, createdAt, updatedAt },
-  } = request;
+	const {
+		query: { displayName, description, createdAt, updatedAt }
+	} = request;
 
-  const filter = buildFilter({
-    displayName,
-    description,
-    createdAt,
-    updatedAt,
-  });
+	const filter = buildFilter({
+		displayName,
+		description,
+		createdAt,
+		updatedAt
+	});
 
-  const companies = await Company.find(filter.length ? { $and: filter } : {});
+	const companies = await Company.find(filter.length ? { $and: filter } : {});
 
-  response.send(companies);
+	response.send(companies);
+});
+
+// gets one company by company ID
+router.get('/:id', async (req, res) => {
+	const { id } = req.params;
+	try {
+		const company = await Company.findById(id);
+
+		if (!company) {
+			res.status(404).send({ error: 'Company not found!' });
+		} else {
+			res.json(company);
+		}
+	} catch (err) {
+		res.status(500).send({ error: "Something went wrong; we're on it!" });
+	}
+});
+
+router.post('/', async (req, res) => {
+	const { displayName, logoUrl, location, description, notes } = req.body;
+	let newContactInfo = dropUndefined({ displayName, logoUrl, location, description, notes });
+
+	try {
+		let newCompany = await Company.create(newContactInfo);
+
+		res.json(newCompany);
+	} catch (err) {
+		let filter = buildFilter({ displayName });
+		let duplicateCompany= await Company.find(filter.length ? { $or: filter } : {});
+
+		if (duplicateCompany) {
+			res.status(409).json(duplicateCompany);
+		} else {
+			res.status(500).send({ error: "Something went wrong; we're on it!" });
+		}
+	}
+});
+
+router.put('/:id', async (req, res) => {
+	const { id } = req.params;
+	const { displayName, logoUrl, location, description, notes } = req.body;
+	let fieldsToUpdate = dropUndefined({ displayName, logoUrl, location, description, notes });
+
+	let updatedCompany = await Company.findByIdAndUpdate(id, fieldsToUpdate, { new: true });
+
+	if (!updatedCompany) res.status(404).send({ error: 'Company not found!' });
+	else res.json(updatedCompany);
+});
+
+router.delete('/:id', async (req, res) => {
+	const { id } = req.params;
+	let deletedCompany = await Company.findByIdAndRemove(id);
+	if (!deletedCompany) res.status(404).send({ error: 'Company not found!' });
+	else res.json(deletedCompany);
 });
 
 module.exports = router;
