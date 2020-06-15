4cd4d26692934d019f82e4d7d66545a816ffce96
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-06-14 19:12:41 -0700

    Added JavaScript to expand textarea height as it fills with content

--- client/src/components/JobTracking/Job.js
+++ client/src/components/JobTracking/Job.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import NavBar from '../NavBar';
 import CompanyInfo from './subComponents/CompanyInfo';
 import StatusBar from './subComponents/StatusBar';
@@ -8,6 +8,31 @@ import InterviewCard from './subComponents/Interview';
 import OfferCard from './subComponents/Offer';
 
 function Saved() {
+
+  useEffect(() => {
+    // Textarea height expansion
+    var autoExpand = function (field) {
+      // Reset field height
+      field.style.height = 'inherit';
+      // Get the computed styles for the element
+      var computed = window.getComputedStyle(field);
+      // Calculate the height
+      var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+                   + parseInt(computed.getPropertyValue('padding-top'), 10)
+                   + field.scrollHeight
+                   + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+                   + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+    
+      field.style.height = height + 'px';
+    
+    };
+    document.addEventListener('input', function (event) {
+      if (event.target.tagName.toLowerCase() !== 'textarea') return;
+      autoExpand(event.target);
+    }, false);
+
+  });
+
   return (
     <div className="job">
       <NavBar />
--- client/src/components/JobTracking/subComponents/CompanyInfo.js
+++ client/src/components/JobTracking/subComponents/CompanyInfo.js
@@ -5,26 +5,17 @@ function CompanyInfo() {
     <div className="col s12 m12 l12">
       <div className="row card-content company-header">
         <div className="card-inner job-company">
-          <div className="col m2 l2 company-image">
+          <div className="col s2 m2 l2 company-image">
             <i className="company-img-src material-icons">add_a_photo</i>
           </div>
-          <div className="co8 m8 l8 company-details">
-            <h3>Company</h3>
-            <p>Job Title</p>
-            <p>City, State</p>
+          <div className="col s8 m8 l8 company-details">
+            <input className="company-input" id="company-name" placeholder="Company Name"></input>
+            <input className="company-input" id="company-city" placeholder="City"></input>
+            <input className="company-input" id="company-state" placeholder="State"></input>
           </div>
-          <div className="col m2 l2 btn-status">
+          <div className="col s2 m2 l2 btn-status">
             <button className="btn-saved">Saved</button>
           </div>
-        </div>
-      </div>
-      <div className="card companyInfo">
-        <div>
-          <input className="col s6 m6 l6" placeholder="Company Name"></input>
-          <input className="col s6 m6 l6" placeholder="Street Address"></input>
-          <input className="col s6 m3 l3" placeholder="City"></input>
-          <input className="col s6 m3 l3" placeholder="State"></input>
-          <input className="col s6 m6 l6" placeholder="ZIP"></input>
         </div>
       </div>
     </div>
