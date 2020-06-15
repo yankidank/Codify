3efd8450cd1577680c70f0ea870c64b07abe90bd
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-06-14 20:12:08 -0700

    Company details input field style updates

--- client/src/components/JobTracking/subComponents/CompanyInfo.js
+++ client/src/components/JobTracking/subComponents/CompanyInfo.js
@@ -5,15 +5,18 @@ function CompanyInfo() {
     <div className="col s12 m12 l12">
       <div className="row card-content company-header">
         <div className="card-inner job-company">
-          <div className="col s2 m2 l2 company-image">
+          <div className="col s12 m2 l2 company-image">
             <i className="company-img-src material-icons">add_a_photo</i>
           </div>
-          <div className="col s8 m8 l8 company-details">
-            <input className="company-input" id="company-name" placeholder="Company Name"></input>
-            <input className="company-input" id="company-city" placeholder="City"></input>
-            <input className="company-input" id="company-state" placeholder="State"></input>
+          <div className="col s12 m6 l7 company-details">
+            <div className="row">
+              <input className="col s12 m12 l12 company-input" id="company-name" placeholder="Company Name"></input>
+              <input className="col s12 m12 l12 company-input" id="company-jobtitle" placeholder="Position Title"></input>
+              <input className="col s12 m5 l4 company-input" id="company-city" placeholder="City"></input>
+              <input className="col s12 m5 l4 company-input" id="company-state" placeholder="State"></input>
+            </div>
           </div>
-          <div className="col s2 m2 l2 btn-status">
+          <div className="col s12 m4 l3 btn-status">
             <button className="btn-saved">Saved</button>
           </div>
         </div>
