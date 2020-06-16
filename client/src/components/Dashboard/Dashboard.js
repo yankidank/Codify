0b0090e9140e011c1196908c3e260acf0a0bb0fe
elliotfouts <elliotfouts@gmail.com>
2020-06-16 10:54:08 -0700

    added async data retrieval to dashbord component

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DoughtnutChart from './Doughnut';
 import List from '../JobList/List';
 import NavBar from '../NavBar';
+import axiosInstance from '../../utils/API';
 
 function Dashboard() {
-  return (
-    <div>
-      <NavBar />
-      <div className="container pushtop dashboard">
-        <div className="row">
-          <DoughtnutChart />
-          <List cols="col s12 m12 l8" />
-        </div>
-      </div>
-    </div>
-  );
+	const [companies, setCompanies] = useState([]);
+	const [chat, setChart] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+        const companyArr = await axiosInstance.get('/api/companies');
+        const jobArr = await axiosInstance.get('api/jobs');
+
+        console.log(jobArr);
+				console.log(companies);
+			} catch (error) {}
+		};
+
+		fetchData();
+	});
+	return (
+		<div>
+			<NavBar />
+			<div className="container pushtop dashboard">
+				<div className="row">
+					<DoughtnutChart />
+					<List cols="col s12 m12 l8" />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Dashboard;
