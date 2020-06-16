99460c3bed1a7d89dcfa81f737ec10d5c39b8fc6
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-06-16 09:45:58 -0700

    Beginning work on Homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import Dashboard from './Dashboard/Dashboard';
+import NavBar from './NavBar';
+import StatusBar from './JobTracking/subComponents/StatusBar';
 
 function Home() {
   return (
     <div className="home">
-      <Dashboard />
+      <NavBar />
+      <div className="container pushtop dashboard">
+        <div className="row">
+          <StatusBar state="1" first="Signup" second="Track" third="Apply" fourth="Employed" />
+          <div className="col s12 m12 l12">
+            <div className="card card-home">
+             <h3>Begin Your Hunt</h3>
+             <p>Track your favorite job posts, application status, contacts, interviews, and offers. Keep organized throughout your job hunt and stay ahead of the game!</p>
+             <p>Sign up and access our job tracking tools for free.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/JobTracking/subComponents/StatusBar.js
+++ client/src/components/JobTracking/subComponents/StatusBar.js
@@ -1,57 +1,107 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function StatusBar() {
+function StatusBar(props) {
+  const state = props.state;
+  const stateClass = {};
+  const activeClass = {};
+  if (state === '1'){
+    stateClass.one = 'col s6 m6 l6 circle-connection circle-connect-await'
+    stateClass.two = 'col s6 m6 l6 circle-connection circle-connect-await'
+    stateClass.three = 'col s6 m6 l6 circle-connection circle-connect-await'
+    stateClass.four = 'col s6 m6 l6 circle-connection circle-connect-await'
+    stateClass.five = 'col s6 m6 l6 circle-connection circle-connect-await'
+    stateClass.six = 'col s6 m6 l6 circle-connection circle-connect-await'
+    activeClass.one = 'col s3 m3 l3 circle-active';
+    activeClass.two = 'col s3 m3 l3 circle-await';
+    activeClass.three = 'col s3 m3 l3 circle-await';
+    activeClass.four = 'col s3 m3 l3 circle-await';
+  } else if (state === '2'){
+    stateClass.one = 'col s6 m6 l6 circle-connection'
+    stateClass.two = 'col s6 m6 l6 circle-connection'
+    stateClass.three = 'col s6 m6 l6 circle-connection circle-connect-await'
+    stateClass.four = 'col s6 m6 l6 circle-connection circle-connect-await'
+    stateClass.five = 'col s6 m6 l6 circle-connection circle-connect-await'
+    stateClass.six = 'col s6 m6 l6 circle-connection circle-connect-await'
+    activeClass.one = 'col s3 m3 l3';
+    activeClass.two = 'col s3 m3 l3 circle-active';
+    activeClass.three = 'col s3 m3 l3 circle-await';
+    activeClass.four = 'col s3 m3 l3 circle-await';
+  } else if (state === '3'){
+    stateClass.one = 'col s6 m6 l6 circle-connection'
+    stateClass.two = 'col s6 m6 l6 circle-connection'
+    stateClass.three = 'col s6 m6 l6 circle-connection'
+    stateClass.four = 'col s6 m6 l6 circle-connection'
+    stateClass.five = 'col s6 m6 l6 circle-connection circle-connect-await'
+    stateClass.six = 'col s6 m6 l6 circle-connection circle-connect-await'
+    activeClass.one = 'col s3 m3 l3';
+    activeClass.two = 'col s3 m3 l3';
+    activeClass.three = 'col s3 m3 l3 circle-active';
+    activeClass.four = 'col s3 m3 l3 circle-await';
+  } else if (state === '4'){
+    stateClass.one = 'col s6 m6 l6 circle-connection'
+    stateClass.two = 'col s6 m6 l6 circle-connection'
+    stateClass.three = 'col s6 m6 l6 circle-connection'
+    stateClass.four = 'col s6 m6 l6 circle-connection'
+    stateClass.five = 'col s6 m6 l6 circle-connection'
+    stateClass.six = 'col s6 m6 l6 circle-connection'
+    activeClass.one = 'col s3 m3 l3';
+    activeClass.two = 'col s3 m3 l3';
+    activeClass.three = 'col s3 m3 l3';
+    activeClass.four = 'col s3 m3 l3 circle-active';   
+  }
+  console.log(stateClass)
+
   return (
     <div className="col s12 m12 l12">
       <div className="row statusBar">
-        <div className="col s3 m3 l3">
-          <p>Saved</p>
+        <div className={activeClass.one}>
+          <p>{props.first}</p>
           <div className="circle-status">
             <div className="row">
               <div className="col s6 m6 l6 circle-connection circle-inactive">
               </div>
-              <div className="col s6 m6 l6 circle-connection">
+              <div className={stateClass.one}>
               </div>
             </div>
           </div>
-          <div className="circle-inner" id="circle-applied"></div>
+          <div className="circle-inner" id="circle-one"></div>
         </div>
-        <div className="col s3 m3 l3 circle-active">
-          <p>Applied</p>
+        <div className={activeClass.two}>
+          <p>{props.second}</p>
           <div className="circle-status">
             <div className="row">
-              <div className="col s6 m6 l6 circle-connection">
+              <div className={stateClass.two}>
               </div>
-              <div className="col s6 m6 l6 circle-connection circle-connect-await">
+              <div className={stateClass.three}>
               </div>
             </div>
           </div>
-          <div className="circle-inner" id="circle-applied"></div>
+          <div className="circle-inner" id="circle-two"></div>
         </div>
-        <div className="col s3 m3 l3 circle-await">
-          <p>Interview</p>
+        <div className={activeClass.three}>
+          <p>{props.third}</p>
           <div className="circle-status">
             <div className="row">
-              <div className="col s6 m6 l6 circle-connection circle-connect-await">
+              <div className={stateClass.four}>
               </div>
-              <div className="col s6 m6 l6 circle-connection circle-connect-await">
+              <div className={stateClass.five}>
               </div>
             </div>
           </div>
-          <div className="circle-inner" id="circle-interview"></div>
+          <div className="circle-inner" id="circle-three"></div>
         </div>
-        <div className="col s3 m3 l3 circle-await">
-          <p>Offer</p>
+        <div className={activeClass.four}>
+          <p>{props.fourth}</p>
           <div className="circle-status">
             <div className="row">
-              <div className="col s6 m6 l6 circle-connection circle-connect-await">
+              <div className={stateClass.six}>
               </div>
               <div className="col s6 m6 l6 circle-connection circle-inactive">
               </div>
             </div>
           </div>
-          <div className="circle-inner" id="circle-offer"></div>
+          <div className="circle-inner" id="circle-four"></div>
         </div>
       </div>
     </div>
