8fb5bd2d882cdc674d8e656dddfd5b755034cfd8
Ana Valdivia <anav1317@yahoo.com>
2020-06-17 12:03:45 -0700

    trying to post a job

--- client/src/components/LoginMenu/AddJob.js
+++ client/src/components/LoginMenu/AddJob.js
@@ -5,11 +5,15 @@ import NavBar from '../NavBar';
 function AddJob() {
   const [post, setPost] = useState({});
 
-  // // Hitting the Post endpoint
-  // const handleAdd = () => {
-  //   axios.post('/api/jobs', { post });
-  //   // window.open('/jobs', '_self');
-  // };
+  // Hitting the Post endpoint
+  const handleAdd = async () => {
+    try {
+      const newJob = await postJob(companyName, position, city, state, url)
+    }
+    catch (error){
+      console.log(error)
+    }
+  }
 
   const onPostInput = event => {
     const { target: { name, value }} = event;
@@ -46,6 +50,22 @@ function AddJob() {
       <NavBar />
       <div>
         <ul className="menuNav">
+          <li>
+            <label for="companyName">Company Name</label>
+            <input id="companyName"></input>
+          </li>
+          <li>
+            <label for="position">Position</label>
+            <input id="position"></input>
+          </li>
+          <li>
+            <label for="city">City</label>
+            <input id="city"></input>
+          </li>
+          <li>
+            <label for="state">State</label>
+            <input id="state"></input>
+          </li>
           <li className="btn-home-login">Job Post URL</li>
           <li>
             <input
