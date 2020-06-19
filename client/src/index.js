2718d05e1c9908316144504329419cbc8d5fc7ec
millernj <noahjmiller17@gmail.com>
2020-06-19 13:11:20 -0700

    adding service worker registration

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { config } from 'dotenv';
+import * as serviceWorker from './serviceWorker';
 
 config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.register();
