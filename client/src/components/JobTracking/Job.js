51a4bdae7f1fe06f6b535fa09370005a61ac13bb
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-06-20 03:40:36 -0700

    Removed texarea height js from useEffect

--- client/src/components/JobTracking/Job.js
+++ client/src/components/JobTracking/Job.js
@@ -16,32 +16,33 @@ function Saved() {
 
 	useEffect(() => {
 		setJobId(id);
-		// Textarea height expansion
-		var autoExpand = function (field) {
-			// Reset field height
-			field.style.height = 'inherit';
-			// Get the computed styles for the element
-			var computed = window.getComputedStyle(field);
-			// Calculate the height
-			var height =
-				parseInt(computed.getPropertyValue('border-top-width'), 10) +
-				parseInt(computed.getPropertyValue('padding-top'), 10) +
-				field.scrollHeight +
-				parseInt(computed.getPropertyValue('padding-bottom'), 10) +
-				parseInt(computed.getPropertyValue('border-bottom-width'), 10);
-
-			field.style.height = height + 'px';
-		};
-		document.addEventListener(
-			'input',
-			function (event) {
-				if (event.target.tagName.toLowerCase() !== 'textarea') return;
-				autoExpand(event.target);
-			},
-			false
-		);
 	});
 
+	// Textarea height expansion
+	var autoExpand = function (field) {
+		// Reset field height
+		field.style.height = 'inherit';
+		// Get the computed styles for the element
+		var computed = window.getComputedStyle(field);
+		// Calculate the height
+		var height =
+			parseInt(computed.getPropertyValue('border-top-width'), 10) +
+			parseInt(computed.getPropertyValue('padding-top'), 10) +
+			field.scrollHeight +
+			parseInt(computed.getPropertyValue('padding-bottom'), 10) +
+			parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+
+		field.style.height = height + 'px';
+	};
+	document.addEventListener(
+		'input',
+		function (event) {
+			if (event.target.tagName.toLowerCase() !== 'textarea') return;
+			autoExpand(event.target);
+		},
+		false
+	);
+	
 	return (
 		<div className="job">
 			<NavBar />
