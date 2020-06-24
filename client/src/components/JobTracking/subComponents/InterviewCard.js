a1fc61ebac6121c0cce9c6226db65a5e765a2bae
elliotfouts <elliotfouts@gmail.com>
2020-06-24 12:34:36 -0700

    fixed random linting error

--- client/src/components/JobTracking/subComponents/InterviewCard.js
+++ client/src/components/JobTracking/subComponents/InterviewCard.js
@@ -4,7 +4,7 @@ import Cleave from 'cleave.js/react';
 import M from "materialize-css";
 
 function InterviewCard(props) {
-  const {handleInputChange, handleSelectChange, time, addNewInterview, date, remote, street, city, state, notes, index, _id} = props;
+  const {handleInputChange, time, addNewInterview, date, remote, street, city, state, notes, index, _id} = props;
 
   useEffect(() => {
     // Change Status Menu
@@ -28,7 +28,7 @@ function InterviewCard(props) {
             <Cleave options={{time: true, timePattern: ['h', 'm']}} className="col s6 m4 l4" placeholder="10:30" onChange={(event) => handleInputChange(event, index, _id)} name="time" value={time}/>
           </div>
           <div className="input-field col s6 m4 l4">
-            <select placeholder="Remote" id="remote" onChange={(event) => handleSelectChange(event, index, _id)}value={remote}>
+            <select placeholder="Remote" id="remote" value={remote}>
               <option value="true">Remote</option>
               <option value="false">Local</option>
             </select>
