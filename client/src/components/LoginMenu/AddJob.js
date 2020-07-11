9fc8c1268748e9724c3bbe31a4e64314a6a8ab39
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-07-10 22:30:46 -0700

    Proof of concept to fill the Add Job fields automatically. End product will present a loading message or button.

--- client/src/components/LoginMenu/AddJob.js
+++ client/src/components/LoginMenu/AddJob.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import NavBar from '../NavBar';
 import {addJob} from "../../utils/API"
+import M from "materialize-css";
+require('dotenv').config();
 
 function AddJob() {
   const [post, setPost] = useState({
@@ -11,6 +13,54 @@ function AddJob() {
     status: "",
     url: ""
   });
+
+  var getPost = async function (url) {
+    // Get the post data
+    const puppeteerDomain = process.env.DOMAIN || 'http://localhost';
+    var puppeteerPort = process.env.PUPPETEER_PORT || 4000;
+    puppeteerPort = puppeteerPort.toString();
+    const puppeteerScrape = 'scrape';
+    const puppeteerUrl = puppeteerDomain+':'+ puppeteerPort +'/'+ puppeteerScrape +'?url='+url;
+    //console.log(puppeteerUrl)
+    var postResp = await fetch(puppeteerUrl);
+    var postObj = await postResp.json();
+    //console.table(postObj);
+    let {company, position, city, state, description} = postObj;
+    setPost({
+      ...post, 
+      companyName: company, 
+      position: position,
+      city: city,
+      state: state,
+      notes: description,
+      url: url
+    });
+    const inputCompanyName = document.getElementById('inputCompanyName');
+    inputCompanyName.value = postObj.company;
+    const inputPosition = document.getElementById('inputPosition');
+    inputPosition.value = postObj.position;
+    const inputCity = document.getElementById('inputCity');
+    inputCity.value = postObj.city;
+    const inputState = document.getElementById('inputState');
+    inputState.value = postObj.state;
+    M.toast({ html: 'Data Imported from Clipboard URL' });
+  };
+
+  var fetchClipboard = async function () {
+    navigator.clipboard
+    .readText()
+    .then(text => {
+      const pasteText = text.trim();
+      // Check that the clipboard holds a link
+      const checkUrl = pasteText.startsWith('http');
+      if (checkUrl) {  
+        getPost(pasteText);        
+      }
+    })
+    .catch(err => {
+      console.log('Something went wrong', err);
+    });
+  }
 
   // Hitting the Post endpoint
   const handleAdd = async (e) => {
@@ -29,33 +79,15 @@ function AddJob() {
 
   const onPostInput = event => {
     const { target: { name, value }} = event;
-
     setPost({ ...post, [name]: value})
   }
 
   useEffect(() => {
     // handleAdd();
-    // Paste Job URL
-    const paste = document.getElementById('paste');
-    paste.addEventListener('click', () => {
-      if (!paste.value) {
-        // Attempt to read clipboard text
-        navigator.clipboard
-          .readText()
-          .then(text => {
-            const pasteText = text.trim();
-            // Check that the clipboard holds a link
-            const checkUrl = pasteText.startsWith('http');
-            if (checkUrl) {
-              setPost({...post, url: pasteText })
-              // paste.value = pasteText;
-            }
-          })
-          .catch(err => {
-            console.log('Something went wrong', err);
-          });
-      }
-    });
+
+    // Attempt to read clipboard text
+    window.addEventListener('load', fetchClipboard);
+
   });
 
   return (
@@ -70,26 +102,16 @@ function AddJob() {
         <div className="card-add-job card card-padded">
           <div className="row">
             <div className="col s12 m12 l6">
-              <input className="menu-input-field" placeholder="Company Name" name="companyName" onChange={onPostInput}></input>
+              <input className="menu-input-field" placeholder="Company Name" name="companyName" id="inputCompanyName" onChange={onPostInput}></input>
             </div>
             <div className="col s12 m12 l6">
-              <input className="menu-input-field" placeholder="Position" name="position" onChange={onPostInput}></input>
+              <input className="menu-input-field" placeholder="Position" name="position" id="inputPosition" onChange={onPostInput}></input>
             </div>
             <div className="col s6 m6 l6">
-              <input className="menu-input-field" placeholder="City" name="city" onChange={onPostInput}></input>
+              <input className="menu-input-field" placeholder="City" name="city" id="inputCity" onChange={onPostInput}></input>
             </div>
             <div className="col s6 m6 l6">
-              <input className="menu-input-field" placeholder="State" name="state" onChange={onPostInput}></input>
-            </div>
-            <div className="col s12 m12 l12">
-              <input
-                className="menu-input-field"
-                id="paste"
-                name="url"
-                placeholder="https://"
-                onChange={onPostInput}
-                value={post.url}
-              ></input>
+              <input className="menu-input-field" placeholder="State" name="state" id="inputState" onChange={onPostInput}></input>
             </div>
             <div className="col s12">
               <a href="/jobs/add" className="button btn-job-add" onClick={handleAdd}>
